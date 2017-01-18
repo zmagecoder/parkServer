acda4344dbec8138ee93a12bd733345f441b9b6a
zmagesky <731452164@qq.com>
2017-01-18 16:24:24 +0800

    q

--- src/main/java/com/mage/park/home/service/HomeService.java
+++ src/main/java/com/mage/park/home/service/HomeService.java
@@ -1,12 +1,15 @@
 package com.mage.park.home.service;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HomeService {
 	
+	private Logger logger = Logger.getLogger(this.getClass());
+	
 	public void test(){
-		System.out.println("====================>>>");
+		logger.info("===============>>>");
 	}
 	
 }
